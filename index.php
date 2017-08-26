15c95309564b690162245d3009c80e9db6755c76
chaow porkaew <chaow.po@up.ac.th>
2017-08-26 14:26:39 +0700

    first init

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+
+<body>
+<h1> สวัสดีครับ </h1>
+
+<?php
+echo "HELLO WORLD!";
+echo "สวัสดี";
+
+$name = "John Doe";
+?>
+
+<h2>สวัสดี <?=$name ?></h2>
+XXXXXXX <br/>
+YYYYYYY
+
+</body>
+</html>
