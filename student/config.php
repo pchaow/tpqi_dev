092ccb322cde530253d8f9f4aeed590c38a47a98
chaow porkaew <chaow.po@up.ac.th>
2017-08-26 14:57:21 +0700

    add connect.php

---
+++ student/config.php
@@ -0,0 +1,18 @@
+<?php
+
+# 1. Declare variables for connection db.
+$host = "127.0.0.1";
+$db = "student";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($host,$username,$password,$db);
+
+if( mysqli_connect_errno() ){
+	echo "Failed to connect to MYSQL: " . mysqli_connect_error();
+	die();
+}
+
+
+
+?>
