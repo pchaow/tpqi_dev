d064669f4abbaf84f090e449712ed23f1e808f54
chaow porkaew <chaow.po@up.ac.th>
2017-08-26 15:22:27 +0700

    update index

--- student/index.php
+++ student/index.php
@@ -1,5 +1,11 @@
 <?php
 	require_once "config.php";
+
+	$sql = "SELECT * FROM students";
+	$query = mysqli_query($conn,$sql);
+
+	$num_student = mysqli_num_rows($query);
+
 ?>
 <html>
 <head>
@@ -7,7 +13,32 @@
 </head>
 <body>
 
-<?php echo "HELLO" ?>
+<h1>รายการนักเรียน</h1>
+<b>มีจำนวนนักเรียน <?=$num_student?> คน</b>
+<br/><br/>
+<table>
+<tr>
+	<th>ID</th>
+	<th>First Name</th>
+	<th>Last Name</th>
+	<th>Age</th>
+	<th>Tel.</th>
+	<th>การจัดการ</th>
+</tr>
+
+<tr>
+	<td>ID</td>
+	<td>First Name</td>
+	<td>Last Name</td>
+	<td>Age</td>
+	<td>Tel.</td>
+	<td>
+		แก้ไข
+
+		ลบ
+	</td>
+</tr>
+</table>
 
 </body>
 </html>
